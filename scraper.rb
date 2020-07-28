2e5828022ae11271e7087ce19b7a3ee9c4c44a9b
leandro brito <leandroabrito89@gmail.com>
2020-07-28 13:15:43 -0300

    add scrape with pagination code

--- scraper.rb
+++ scraper.rb
@@ -20,3 +20,32 @@ end
 
 scraper
 
+
+
+# def scraper
+#   url = "https://blockwork.cc/"
+#   unparsed_page = HTTParty.get(url)
+#   parsed_page = Nokogiri::HTML(unparsed_page)
+#   jobs = []
+#   jobs_listing = parsed_page.css('div.listingCard')
+#   page = 1
+#   per_page = jobs_listing.count
+#   total = parsed_page.css('div.job-count').text.split(' ')[1].gsub(',', '').to_i
+#   last_page = (total.to_f / per_page.to_f).round
+#   while page <= last_page
+#     pagination_url = "https://blockwork.cc/listings?page#{page}"
+#     pagination_unparsed_page = HTTParty.get(pagination_url)
+#     pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page)
+#     pagination_jobs_listing = pagination_parsed_page.css('div.listingCard')
+#     pagination_jobs_listing.each do |job_listing|
+#       job = {
+#         title: job.listing.css('span.job-title').text,
+#         company: job_listing.css('span.company').text,
+#         location: job_listing.css('span.location').text,
+#         url: "https://blockwork.cc" + job_listing.css('a')[0].attributes["href"].value
+#       }
+#       jobs << job
+#     end
+#     page ++
+#   end
+# end
